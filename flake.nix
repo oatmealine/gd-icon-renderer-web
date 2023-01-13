e1d2d6d3796668652c1926e93af25070aa700dde
Jill "oatmealine" Monoids <oatmealine@disroot.org>
2023-01-13 17:21:37 +0300

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,53 @@
+
+{
+  description = "A server-side renderer API for Geometry Dash icons";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    crystal-flake.url = "github:manveru/crystal-flake";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, crystal-flake }:
+    (with flake-utils.lib; eachSystem defaultSystems) (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        inherit (crystal-flake.packages.${system}) crystal shards;
+      in
+      rec {
+        packages = flake-utils.lib.flattenTree rec {
+          gd-icon-renderer-web = pkgs.crystal.buildCrystalPackage {
+            pname = "gd-icon-renderer-web";
+            version = "0.1.0";
+
+            src = ./.;
+
+            format = "shards";
+            lockFile = ./shard.lock;
+            shardsFile = ./shards.nix;
+
+            buildInputs = with pkgs; [ openssl pkg-config vips ] ++ [ crystal ];
+
+            nativeBuildInputs = with pkgs; [ openssl pkg-config vips ] ++ [ crystal ];
+
+            doInstallCheck = false;
+
+            crystal = crystal;
+          };
+        };
+
+        defaultPackage = packages.gd-icon-renderer-web;
+
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            openssl
+          ];
+
+          nativeBuildInputs = with pkgs; [
+            pkgconfig
+            crystal
+            shards
+          ];
+        };
+      });
+}
---
+++ shards.nix
@@ -0,0 +1,12 @@
+{
+  gd-icon-renderer = {
+    url = "https://github.com/oatmealine/gd-icon-renderer.git";
+    rev = "c8b2deb3a1ac73eccecafefa59d20f57519e20b4";
+    sha256 = "1vwqzl1zfzxp2iwc5hgasj07jfamfkvv11j6bzyllykp4mlp83la";
+  };
+  vips = {
+    url = "https://github.com/naqvis/crystal-vips.git";
+    rev = "381c4ba25b31be4094fe8bc6acf740c13fc595ae";
+    sha256 = "0msgxgz5k42fv0hj26lh4z2m9fmyh8h0994fr3snfkv7d2vla9sn";
+  };
+}
